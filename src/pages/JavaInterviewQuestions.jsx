5b0023c1067691fef7cd43ec9ac16e7a3fb46098
Sumit Rawal <sumit.rawal@txtgroup.com>
2026-08-24 11:26:36 +0700

    Add Java Streams API interview questions PDF to Java interview questions page

--- src/pages/JavaInterviewQuestions.jsx
+++ src/pages/JavaInterviewQuestions.jsx
@@ -21,6 +21,7 @@ export default function JavaInterviewQuestions() {
         { href: '/top-350-java-interview-questions.pdf', label: 'Top 350 Java Interview Questions (PDF)' },
         { href: '/java-interview-help.pdf', label: 'Java Interview Help (PDF)' },
         { href: '/1000-java-interview-questions.pdf', label: '1000 Java Interview Questions (PDF)' },
+        { href: '/java-notes/java_streams_api_interview_questions.pdf', label: 'Java Streams API Interview Questions (PDF)' },
       ]}
       sourceNote={
         <p>
